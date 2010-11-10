ba678553f914bc661503e3627df02985c6376476
Tom Wilson <tom@jackhq.com>
2010-11-09 21:36:30 -0500

    throttled rank check

--- lib/whoru/rank.rb
+++ lib/whoru/rank.rb
@@ -2,10 +2,9 @@ module Whoru
   module Rank
     def rank(friends)
       ranked_friends = []
-      friends.each_with_index do |friend, index|
+      friends[0..1000].each_with_index do |friend, index|
         trust_rank = trstrank(friend)
         ranked_friends << trust_rank unless trust_rank == {}  
-        #break if index > 5
       end
       ranked_friends.sort_by { |t| t['trstrank'] }.reverse
     end
